d7e8f1659fe865c187e149621d83722d287c23d1
pyavchik <pyavchik@gmail.com>
2018-11-16 16:22:41 +0200

    removed screenshotPath from wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -71,7 +71,7 @@ exports.config = {
     bail: 0,
     //
     // Saves a screenshot to a given path if a command fails.
-    screenshotPath: './errorShots/',
+    //screenshotPath: './errorShots/',
     //
     // Set a base URL in order to shorten url command calls. If your `url` parameter starts
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
@@ -112,8 +112,8 @@ exports.config = {
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
     // services: [],//
-    services: ['selenium-standalone'],
-    //services: ['trueautomation'],
+    //services: ['selenium-standalone'],
+    services: ['trueautomation'],
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
     // see also: http://webdriver.io/guide/testrunner/frameworks.html
