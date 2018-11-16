2a31a5a7a1cf880b6f49073def4cd2d73702c6d9
pyavchik <pyavchik@gmail.com>
2018-11-16 13:44:48 +0200

    added test for trueautomation.io

---
+++ test/specs/simple.js
@@ -0,0 +1,15 @@
+// example of a test group
+// note: all tests under the test directory are ran
+
+describe('TrueAutomation.IO page', ()=> {
+    beforeEach(() => {
+        browser.url("http://trueautomation.io");
+    });
+
+    it('should demonstrate the scroll command', function () {
+        $('[href="https://app.trueautomation.io/auth/signin"]').click();
+        $('[href="signup1"]').click();
+        $('[placeholder="Email"]').setValue('your@gmail.com');
+    });
+
+});
